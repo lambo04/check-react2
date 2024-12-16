79aec60e24f74d049f0eb77d7c1bf841accd80bf
lambo04 <omarmidouni0@gmail.com>
2024-12-16 09:18:24 +0100

    fini

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,21 +4,23 @@ import "../App.css"; // Import the custom styles
 
 const Player = ({ imageUrl, name, team, nationality, jerseyNumber, age }) => {
   return (
-    <Card className="player-card">
-      <Card.Img variant="top" src={imageUrl} className="player-card-img" />
-      <Card.Body>
-        <Card.Title className="player-card-title">{name}</Card.Title>
-        <Card.Text className="player-card-text">
-          <strong>Team:</strong> {team}
-          <br />
-          <strong>Nationality:</strong> {nationality}
-          <br />
-          <strong>Jersey Number:</strong> {jerseyNumber}
-          <br />
-          <strong>Age:</strong> {age}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <div>
+      <Card className="player-card">
+        <Card.Img variant="top" src={imageUrl} className="player-card-img" />
+        <Card.Body>
+          <Card.Title className="player-card-title">{name}</Card.Title>
+          <Card.Text className="player-card-text">
+            <strong>Team:</strong> {team}
+            <br />
+            <strong>Nationality:</strong> {nationality}
+            <br />
+            <strong>Jersey Number:</strong> {jerseyNumber}
+            <br />
+            <strong>Age:</strong> {age}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
